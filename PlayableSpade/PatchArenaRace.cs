d59c54d26d116ffc99113efc2f824a0373b6b83a
Kuborros <kubo@catbat.rocks>
2023-03-05 23:46:47 +0100

    Another fix

--- PlayableSpade/PatchArenaRace.cs
+++ PlayableSpade/PatchArenaRace.cs
@@ -9,13 +9,16 @@ namespace PlayableSpade
 {
     internal class PatchArenaRace
     {
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(ArenaRace), "Start", MethodType.Normal)]
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ArenaRace), "Update", MethodType.Normal)]
         static void PatchArenaRaceStart(ArenaRace __instance, ref FPHudDigit ___hudDistanceMarker)
         {
-            if (FPStage.currentStage.GetPlayerInstance_FPPlayer().characterID == (FPCharacterID)5 && ___hudDistanceMarker.digitFrames.Length < 16)
+            if(___hudDistanceMarker.digitFrames.Length < 16)
             {
                 ___hudDistanceMarker.digitFrames = ___hudDistanceMarker.digitFrames.AddToArray(Plugin.moddedBundle.LoadAssetWithSubAssets<Sprite>("Spade_Stock")[0]);
+            }
+            if (FPStage.currentStage.GetPlayerInstance_FPPlayer().characterID == (FPCharacterID)5)
+            {
                 ___hudDistanceMarker.SetDigitValue(15);
             }
         }
